f0499658aa1077f04f40d9c14ac6c7fee161de32
Joe Near <jnear@csail.mit.edu>
2014-10-22 15:57:14 -0400

    everything kinda works

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141022185147) do
+ActiveRecord::Schema.define(version: 20141022190049) do
 
   create_table "images", force: true do |t|
     t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.integer  "image_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
